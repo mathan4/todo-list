583d01dd2c002c1d453308a143d3a52ab6d61e7a
mathan4 <mathansmart5862@gmail.com>
2024-07-03 21:42:23 +0530

    Final commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDoListComponent from './Components/ToDoListComponent/ToDoListComponent'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <ToDoListComponent/>
+    </React.Fragment>
+  )
+}
+
+export default App
